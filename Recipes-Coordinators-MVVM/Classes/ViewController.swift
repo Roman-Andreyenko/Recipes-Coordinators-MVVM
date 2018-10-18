b3a6898cce13d2835e00633fe69c8ed0cb58b7eb
Roman Andreyenko <roman.andreyenko@gmail.com>
2018-10-18 18:45:34 +0300

    Added project skeleton

---
+++ Recipes-Coordinators-MVVM/Classes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipes-Coordinators-MVVM
+//
+//  Created by Roman Andreyenko on 10/18/18.
+//  Copyright © 2018 Roman Andreyenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
