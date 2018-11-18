1421dea1ef41f87534a6711c81952d2728c90ec2
Roman Andreyenko <roman.andreyenko@gmail.com>
2018-11-18 15:50:00 +0300

    added Swinject and SwinjectAutoregistration pods

--- Podfile
+++ Podfile
@@ -8,5 +8,7 @@ target 'Recipes-Coordinators-MVVM' do
     pod 'RxSwift', '4.3.1'
     pod 'RxCocoa', '4.3.1'
     pod 'ReachabilitySwift', '4.1.0'
+    pod 'Swinject', '2.5.0'
+    pod 'SwinjectAutoregistration', '2.5.0'
 
 end
--- Pods/Target Support Files/Pods-Recipes-Coordinators-MVVM/Pods-Recipes-Coordinators-MVVM-frameworks.sh
+++ Pods/Target Support Files/Pods-Recipes-Coordinators-MVVM/Pods-Recipes-Coordinators-MVVM-frameworks.sh
@@ -105,11 +105,15 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwinjectAutoregistration/SwinjectAutoregistration.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Swinject/Swinject.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwinjectAutoregistration/SwinjectAutoregistration.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
